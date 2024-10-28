8ab48fdf78c26abba9a76648c92ccab63ef1a089
chin <149858139+dangkhoa2004@users.noreply.github.com>
2024-10-28 08:24:12 +0700

    Initial commit

---
+++ src/test/java/com/backend/cineboo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cineboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
