93f48bba197b4a31febd013c61d8b8508eb1ce04
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-11-14 17:08:39 +0700

    Fĩx DT002

--- src/main/java/com/backend/cineboo/repository/DoTuoiRepository.java
+++ src/main/java/com/backend/cineboo/repository/DoTuoiRepository.java
@@ -1,9 +1,18 @@
 package com.backend.cineboo.repository;
 
 import com.backend.cineboo.entity.DoTuoi;
+import com.backend.cineboo.entity.PTTT;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DoTuoiRepository  extends JpaRepository<DoTuoi,Long> {
+    @Query(value = "SELECT MAX(ID) FROM Phim",nativeQuery = true)
+    Long getMaxTableId();
+
+    @Query(value = "SELECT * FROM DoTuoi WHERE TenDoTuoi = ? ",nativeQuery=true)
+    Optional<DoTuoi> checkDuplicate(String tenDoTuoi);
 }
