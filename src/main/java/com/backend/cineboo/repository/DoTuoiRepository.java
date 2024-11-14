bb6d52b2d2afd8c551e7512cd96b5aeebce1f0d1
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-11-14 17:25:34 +0700

    Fix DT002

--- src/main/java/com/backend/cineboo/repository/DoTuoiRepository.java
+++ src/main/java/com/backend/cineboo/repository/DoTuoiRepository.java
@@ -2,8 +2,17 @@ package com.backend.cineboo.repository;
 
 import com.backend.cineboo.entity.DoTuoi;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Repository
 public interface DoTuoiRepository  extends JpaRepository<DoTuoi,Long> {
+    @Query(value = "SELECT MAX(ID) FROM DoTuoi",nativeQuery = true)
+    Long getMaxTableId();
+
+    @Query(value = "SELECT * FROM DoTuoi WHERE TenDoTuoi = ? ",nativeQuery = true)
+    Optional<DoTuoi> checkDuplicate(String tenDoTuoi);
 }
