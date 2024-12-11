9894097735208535613e787f792b9aba1b1b6144
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-12-11 10:38:40 +0700

    Xoá view gây lỗi khỏi Database

--- src/main/java/com/backend/cineboo/Application.java
+++ src/main/java/com/backend/cineboo/Application.java
@@ -1,10 +1,7 @@
 package com.backend.cineboo;
 
-import com.backend.cineboo.service.PublicMeBaby;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class Application {
