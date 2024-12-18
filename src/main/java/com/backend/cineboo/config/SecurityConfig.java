7122e7bc84946f24efd183c679996c3341d4f89d
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-12-18 17:18:09 +0700

    Security/PhanQuyen/Final(maybe)

--- src/main/java/com/backend/cineboo/config/SecurityConfig.java
+++ src/main/java/com/backend/cineboo/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 public class SecurityConfig {
     @Autowired
@@ -36,65 +37,90 @@ public class SecurityConfig {
                         "/swagger-resources/**",
                         "/swagger-resources",
                         "/swagger-ui/**").permitAll()
-//                .requestMatchers("/api/**").permitAll()
-//                .requestMatchers("/api/user/login").permitAll()
-//// API Prefix: /phim
-//                .requestMatchers("/phim/get", "/phim/find/**").permitAll()
-//                .requestMatchers("/phim/add").hasAnyAuthority("Quản trị viên")
-//                .requestMatchers("/phim/update/**", "/phim/disable/**").hasAnyAuthority("Quản lý", "Quản trị viên")
+                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/api/user/login").permitAll()
+// API Prefix: /phim
+                .requestMatchers("/phim/get", "/phim/find/**").permitAll()//done
+                .requestMatchers("/phim/add").hasAnyAuthority(ERoles.QL, ERoles.QTV)//done
+                .requestMatchers("/phim/update/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)//done
+                .requestMatchers("/phim/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)//done
 //
-//// API Prefix: /hoadon
-//                .requestMatchers("/hoadon/add").hasAnyAuthority("Nhân viên bán vé", "Quản lý", "Quản trị viên", "KhachHang")
-//                .requestMatchers("/hoadon/get/**", "/hoadon/find/**").hasAnyAuthority("KhachHang", "Nhân viên bán vé", "Quản lý", "Quản trị viên")
-//                .requestMatchers("/hoadon/update/**").hasAnyAuthority("Nhân viên bán vé", "Quản lý", "Quản trị viên")
-//                .requestMatchers("/hoadon/cancel").hasAnyAuthority("KhachHang")
-//                .requestMatchers("/hoadon/cancel/confirm").hasAnyAuthority("Nhân viên kế toán")
-//                .requestMatchers("/hoadon/disable/**").hasAnyAuthority("Quản lý", "Quản trị viên")
+// API Prefix: /hoadon
+                .requestMatchers("/hoadon/add").hasAnyAuthority(CRoles.GENERAL)
+                .requestMatchers("/hoadon/get","/hoadon/get/**").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_TN)
+                .requestMatchers("/hoadon/find/**").hasAnyAuthority(CRoles.GENERAL, ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_TN)
+                .requestMatchers("/hoadon/update/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
+                .requestMatchers("/hoadon/cancel/**").hasAnyAuthority(CRoles.GENERAL)
+                .requestMatchers("/hoadon/cancel/confirm").hasAnyAuthority(ERoles.NV_KT_TOAN)
+                .requestMatchers("/hoadon/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
+                .requestMatchers("/hoadon/price/**").permitAll()
+                .requestMatchers("/hoadon/print/**").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_BV)//done
+                .requestMatchers("/hoadon/status/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
+                .requestMatchers("/hoadon/download/**").permitAll()//done
 //
 //// API Prefix: /khachhang
-//                .requestMatchers("/khachhang/add").hasAnyAuthority("Quản lý", "Quản trị viên")
-//                .requestMatchers("/khachhang/get/**", "/khachhang/find/**").hasAnyAuthority("Quản lý", "Quản trị viên")
-//                .requestMatchers("/khachhang/update/**", "/khachhang/disable/**").hasAnyAuthority("Quản lý", "Quản trị viên")
-//
+                .requestMatchers("/khachhang/add").permitAll()
+                .requestMatchers("/khachhang/find/**").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_BV, ERoles.NV_TN, CRoles.GENERAL)
+                .requestMatchers("/khachhang/get/**").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_BV, ERoles.NV_TN)
+                .requestMatchers("/khachhang/get").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_BV, ERoles.NV_TN)
+                .requestMatchers("/khachhang/update/**", "/khachhang/disable/**").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                .requestMatchers("/khachhang/update", "/khachhang/disable").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+
 //// API Prefix: /voucher
-//                .requestMatchers("/voucher/get", "/voucher/find/**").hasAnyAuthority("Nhân viên bán vé", "Quản lý", "Quản trị viên", "KhachHang")
-//                .requestMatchers("/voucher/add", "/voucher/update/**", "/voucher/disable/**").hasAnyAuthority("Quản lý", "Quản trị viên")
+                .requestMatchers("/voucher/get", "/voucher/find/**").permitAll()
+                .requestMatchers("/voucher/add", "/voucher/update/**", "/voucher/disable/**").hasAnyAuthority(ERoles.QL,ERoles.QTV)
 //
 //// API Prefix: /theloaiphim
-//                .requestMatchers("/theloaiphim/get", "/theloaiphim/find/**").hasAnyAuthority("Nhân viên bán vé", "Quản lý", "Quản trị viên")
-//                .requestMatchers("/theloaiphim/add", "/theloaiphim/update/**", "/theloaiphim/disable/**").hasAnyAuthority("Quản trị viên")
+                .requestMatchers("/theloaiphim/get", "/theloaiphim/find/**").permitAll()
+                .requestMatchers("/theloaiphim/add", "/theloaiphim/update/**", "/theloaiphim/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
 //
 //// API Prefix: /pttt (Payment Methods)
-//                .requestMatchers("/pttt/get", "/pttt/find/**").hasAnyAuthority("Quản lý", "Quản trị viên")
-//                .requestMatchers("/pttt/add", "/pttt/update/**", "/pttt/disable/**").hasAnyAuthority("Quản trị viên")
+                .requestMatchers("/pttt/get", "/pttt/find/**").permitAll()
+                .requestMatchers("/pttt/add", "/pttt/update/**", "/pttt/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
 //
 //// API Prefix: /dotuoi (Age Restrictions)
-//                .requestMatchers("/dotuoi/get", "/dotuoi/find/**").hasAnyAuthority("Quản trị viên")
-//                .requestMatchers("/dotuoi/add", "/dotuoi/update/**", "/dotuoi/disable/**").hasAnyAuthority("Quản trị viên")
+                .requestMatchers("/dotuoi/get", "/dotuoi/find/**").permitAll()
+                .requestMatchers("/dotuoi/add", "/dotuoi/update/**", "/dotuoi/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
 //
 //// API Prefix: /ghe (Seats)
-//                .requestMatchers("/ghe/get", "/ghe/find/**").hasAnyAuthority("Quản trị viên","KhachHang","Quản lý","Nhân viên bán vé")
-//                .requestMatchers("/ghe/add", "/ghe/update/**", "/ghe/disable/**").hasAnyAuthority("Quản trị viên","KhachHang","Quản lý","Nhân viên bán vé","NhanVien")
-////                .requestMatchers("/ghe/*").permitAll()
-////                .requestMatchers("/ghe/**").permitAll()
+                .requestMatchers("/ghe/get", "/ghe/find/**").hasAnyAuthority(CRoles.GENERAL, ERoles.GENERAL)
+                .requestMatchers("/ghe/findWithBooking/**").hasAnyAuthority(CRoles.GENERAL, ERoles.GENERAL)
+                .requestMatchers("/ghe/add", "/ghe/update/**", "/ghe/disable/**").hasAnyAuthority(ERoles.QTV, ERoles.QL)
 //// API Prefix: /review
-//                .requestMatchers("/review/add").hasAnyAuthority("KhachHang")
-//                .requestMatchers("/review/update/**", "/review/disable/**").hasAnyAuthority("Quản lý", "Quản trị viên")
+                .requestMatchers("/review/add").hasAnyAuthority(CRoles.GENERAL)
+                .requestMatchers("/review/update/**", "/review/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
 //
 //// API Prefix: /thongke (Statistics)
-//                .requestMatchers("/thongke/**").hasAnyAuthority("Quản lý", "Nhân viên Marketing")
+                .requestMatchers("/thongke/**").hasAnyAuthority(ERoles.QL, ERoles.QTV, ERoles.NV_KT_TOAN, ERoles.NV_MKT)
 //
-//// API Prefix: /payos (Payment-related APIs)
-//                .requestMatchers("/payos/create-payment-link/**").permitAll()
-//                .requestMatchers("/payos/cancel/**", "/payos/get/**").permitAll()
-//// API Prefix: /suatchieu (Showtimes)
-////                                .requestMatchers("/suatchieu/get/**", "/suatchieu/find/**","/suatchieu/find/ID_Phim/**").hasAnyAuthority("KhachHang", "Nhân viên bán vé", "Quản lý", "Quản trị viên")
-////                                .requestMatchers("/suatchieu/add", "/suatchieu/update/**", "/suatchieu/disable/**").hasAnyAuthority("Quản trị viên", "Quản lý")
+// API Prefix: /payos (Payment-related APIs)
+                .requestMatchers("/payos/create-payment-link/**").hasAnyAuthority(CRoles.GENERAL)
+                .requestMatchers("/payos/get/**").permitAll()
+                .requestMatchers("/payos/cancel/**").hasAnyAuthority(CRoles.GENERAL, ERoles.NV_BV, ERoles.QL)
+// API Prefix: /suatchieu (Showtimes)
+                .requestMatchers("/suatchieu/get/**", "/suatchieu/find/**", "/suatchieu/find/ID_Phim/**").permitAll()
+                .requestMatchers("/suatchieu/add","/suatchieu/add/**", "/suatchieu/update/**", "/suatchieu/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
+                .requestMatchers("/suatchieu/get").permitAll()
+                // API Prefix: /phongchieu
+                .requestMatchers("/phongchieu/get/**", "/phongchieu/find/**").permitAll()
+                .requestMatchers("/phongchieu/get").permitAll()
+                .requestMatchers("/phongchieu/add", "/phongchieu/update/**", "/phongchieu/disable/**").hasAnyAuthority(ERoles.QL, ERoles.QTV)
+                //API Prefix: /taikhoan
+                .requestMatchers("/taikhoan/**").permitAll()
+                //API Prefix: /nhanvien
+                .requestMatchers("/nhanvien/get").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                .requestMatchers("/nhanvien/add").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                .requestMatchers("/nhanvien/update").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                .requestMatchers("/nhanvien/update/**").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                .requestMatchers("/nhanvien/find").hasAnyAuthority(ERoles.GENERAL)
+                .requestMatchers("/nhanvien/find/**").hasAnyAuthority(ERoles.GENERAL)
+                .requestMatchers("/nhanvien/disable/**").hasAnyAuthority(ERoles.QL,ERoles.QTV)
+                //API Prefix: /hoanve
+                .requestMatchers("/hoanve/**").hasAnyAuthority(ERoles.QL,ERoles.QTV,ERoles.NV_KT_TOAN)
                 .anyRequest().permitAll()
-
                 .and()
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(requestThrottleFilter,TokenAuthenticationFilter.class);
+                .addFilterBefore(requestThrottleFilter, TokenAuthenticationFilter.class);
         return http.build();
     }
 }
